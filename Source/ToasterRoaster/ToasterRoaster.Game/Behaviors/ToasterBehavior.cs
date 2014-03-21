b0f397271c9d01d7de376ac960a7b9caffdbe51f
Simmay <nomis_mayer@web.de>
2014-03-21 11:39:22 +0100

    Added some Behavior

---
+++ Source/ToasterRoaster/ToasterRoaster.Game/Behaviors/ToasterBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaveEngine.Common.Input;
+using WaveEngine.Components.UI;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Services;
+
+namespace ToasterRoaster.Game.Behaviors
+{
+    public class ToasterBehavior : Behavior
+    {
+        private KeyboardState _state;
+
+        [RequiredComponent]
+        public Transform3D ToasterTransform;
+
+        public ToasterBehavior()
+            : base("ToasterBehavior")
+        { }
+                protected override void Update(TimeSpan gameTime)
+        {
+            _state = WaveServices.Input.KeyboardState;
+
+            if (_state.Left == ButtonState.Pressed)
+                ToasterTransform.Rotation.Y -= 0.03f;
+            if (_state.Right == ButtonState.Pressed)
+                ToasterTransform.Rotation.Y += 0.03f;
+
+            //EntityManager.Find<TextBlock>("ToasterPosition").Text = "Toaster Position: " + ToasterTransform.Position.ToString() + "\tRotation: " + ToasterTransform.Rotation.ToString();
+    
+        }
+    }
+}
--- Source/ToasterRoaster/ToasterRoaster.Game/Scenes/GameScene.cs
+++ Source/ToasterRoaster/ToasterRoaster.Game/Scenes/GameScene.cs
@@ -3,35 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToasterRoaster.Game.Behaviors;
 using WaveEngine.Common.Graphics;
 using WaveEngine.Common.Math;
 using WaveEngine.Components.Cameras;
+using WaveEngine.Components.Gestures;
 using WaveEngine.Components.Graphics3D;
+using WaveEngine.Components.UI;
 using WaveEngine.Framework;
 using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Physics2D;
 using WaveEngine.Framework.Physics3D;
+using WaveEngine.Framework.UI;
 using WaveEngine.Materials;
 
 namespace ToasterRoaster.Game.Scenes
 {
-	public class GameScene : Scene
-	{
-		protected override void CreateScene()
-		{
-			RenderManager.BackgroundColor = Color.Green;
-			RenderManager.DebugLines = true;
+    public class GameScene : Scene
+    {
+        protected override void CreateScene()
+        {
+            RenderManager.BackgroundColor = Color.Green;
+            RenderManager.DebugLines = true;
+
+            TextBlock toasterPosition = new TextBlock("ToasterPosition")
+            {
+                Text = "Toaster Position: ",
+                Margin = new Thickness(20f),
+            };
+            EntityManager.Add(toasterPosition);
+
+            var cube = new Entity("cube")
+                .AddComponent(Model.CreateCube())
+                .AddComponent(new MaterialsMap(new BasicMaterial(Color.White)))
+                .AddComponent(new Transform3D())
+                .AddComponent(new ModelRenderer());
+            EntityManager.Add(cube);
 
             CreateLight();
-			CreateCamera();
-			CreateToaster();
+            CreateCamera();
+            CreateToaster();
             //CreatePlate();
-		}
+        }
 
         private void CreatePlate()
         {
             var plate = new Entity("plate")
                 .AddComponent(new Model("Assets/Models/teller_firsttry.wpk"))
-                .AddComponent(new MaterialsMap(new BasicMaterial("Assets/Textures/Plate.wpk") { LightingEnabled = true, DiffuseColor = Color.White,}))
+                .AddComponent(new MaterialsMap(new BasicMaterial("Assets/Textures/Plate.wpk") { LightingEnabled = true, DiffuseColor = Color.White, }))
                 .AddComponent(new Transform3D())
                 .AddComponent(new ModelRenderer());
 
@@ -45,22 +64,33 @@ namespace ToasterRoaster.Game.Scenes
             EntityManager.Add(skylight);
         }
 
-		private void CreateToaster()
-		{
+        private void CreateToaster()
+        {
             var toaster = new Entity("toaster")
                 .AddComponent(new Model("Assets/Models/toaster_template_uv_fbx.wpk"))
                 .AddComponent(new MaterialsMap(new BasicMaterial("Assets/Textures/toaster_red.wpk") { LightingEnabled = true, }))
                 .AddComponent(new Transform3D() { Scale = new Vector3(1.5f, 1, 0.67f), })
-                .AddComponent(new ModelRenderer());
+                .AddComponent(new ModelRenderer())
+                .AddComponent(new ToasterBehavior())
+                .AddComponent(new Transform2D())
+                .AddComponent(new RectangleCollider())
+                .AddComponent(new TouchGestures());
 
-			EntityManager.Add(toaster);
-		}
+            toaster.FindComponent<TouchGestures>().TouchPressed += new EventHandler<GestureEventArgs>(StartGame);
 
-		private void CreateCamera()
-		{
-			FreeCamera mainCamera = new FreeCamera("MainCamera", new Vector3(0, 15, 25), Vector3.Zero);
-			EntityManager.Add(mainCamera);
-			RenderManager.SetActiveCamera(mainCamera.Entity);
-		}
-	}
+            EntityManager.Add(toaster);
+        }
+
+        private void CreateCamera()
+        {
+            //FixedCamera mainCamera = new FixedCamera("MainCamera", new Vector3(0, 15, 25), Vector3.Zero);
+            FreeCamera mainCamera = new FreeCamera("MainCamera", new Vector3(0, 15, 25), Vector3.Zero);
+            EntityManager.Add(mainCamera);
+            RenderManager.SetActiveCamera(mainCamera.Entity);
+        }
+        private void StartGame(object sender, GestureEventArgs e)
+        {
+            EntityManager.Find<TextBlock>("ToasterPosition").Text = "Start";
+        }
+    }
 }
