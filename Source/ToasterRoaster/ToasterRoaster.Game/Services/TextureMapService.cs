f2d5764f1d301d682dc80e34b0aa9795edb8c347
Simmay <nomis_mayer@web.de>
2014-04-10 22:33:33 +0200

    Added TextureMapService

---
+++ Source/ToasterRoaster/ToasterRoaster.Game/Services/TextureMapService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaveEngine.Common;
+
+namespace ToasterRoaster.Game.Services
+{
+    class TextureMapService : Service
+    {
+        public bool[,] GivenTexture { get; set; }
+        public bool[,] DrawnTexture { get; set; }
+
+        protected override void Initialize()
+        {
+        }
+    }
+}
